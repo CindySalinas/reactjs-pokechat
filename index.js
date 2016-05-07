9453519ea7c695a94c2a07d6c1ebbb3b31e4035f
Cindy Salinas <csalinas@socialh4ck.com>
2016-05-07 17:30:35 -0400

    added socket.io to server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import http from 'http';
 import engine from 'socket.io';
+import dbapi from './db-api';
 //constant
 const port = 5000;
 const app = express();
@@ -14,6 +15,20 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/pokemons', (req, res) => {
+	dbapi.pokemons.find((pokemons) => {
+		res.json(pokemons);
+	})
+});
+
 let server = http.createServer(app).listen(port, () => {
 	console.log(`El servidor esta escuchando en el puerto ${port}`);
 });
+
+const io = engine.listen(server);
+
+io.on('connection', (socket) => {
+	socket.on('message', (msg) => {
+		io.emit('message', msg);
+	})
+})
